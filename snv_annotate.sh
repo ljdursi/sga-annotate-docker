e3ceb69ba0cd573c1b2a2befee843fc5503fed62
Jonathan Dursi <jonathan@dursi.ca>
2016-02-01 22:06:36 -0500

    Add requirement for bam indexes

--- snv_annotate.sh
+++ snv_annotate.sh
@@ -18,15 +18,28 @@ readonly NORMAL_BAM=$2
 readonly TUMOUR_BAM=$3
 readonly REFERENCE=${4-$DEFREF}
 
+
+if [ ! -f ${NORMAL_BAM}.bai ] 
+then
+    /usr/local/bin/samtools index ${NORMAL_BAM}
+fi
+
+if [ ! -f ${TUMOUR_BAM}.bai ] 
+then
+    /usr/local/bin/samtools index ${TUMOUR_BAM}
+fi
+
 # output readcounts from input vcf
 
 readonly NORMAL_READCOUNTS=${INPUT_VCF/.vcf/.normal.rc}
 readonly TUMOUR_READCOUNTS=${INPUT_VCF/.vcf/.tumour.rc}
 
-/usr/bin/bam-readcount --reference-fasta ${REFERENCE} --site-list <( zcat ${INPUT_VCF} | awk '{ printf "%s\t%d\t%d\n",$1,$2,$2+1 }' ) \
-    --max-count 8000 --max-warnings 0 $NORMAL_BAM > ${NORMAL_READCOUNTS}
+/usr/bin/bam-readcount --reference-fasta ${REFERENCE} \
+    --site-list <( zcat ${INPUT_VCF} | awk '$1 !~ /^#/{ printf "%s\t%d\t%d\n",$1,$2,$2+1 }' ) \
+    --max-count 8000 $NORMAL_BAM > ${NORMAL_READCOUNTS} 2> /dev/null
 
-/usr/bin/bam-readcount --reference-fasta ${REFERENCE} --site-list <( zcat ${INPUT_VCF} | awk '{ printf "%s\t%d\t%d\n",$1,$2,$2+1 }' ) \
-    --max-count 8000 --max-warnings 0 $TUMOUR_BAM > ${TUMOUR_READCOUNTS}
+/usr/bin/bam-readcount --reference-fasta ${REFERENCE} \
+    --site-list <( zcat ${INPUT_VCF} | awk '$1 !~ /^#/{ printf "%s\t%d\t%d\n",$1,$2,$2+1 }' ) \
+    --max-count 8000 $TUMOUR_BAM > ${TUMOUR_READCOUNTS} 2> /dev/null
 
 /usr/local/bin/annotate_from_readcounts.py ${INPUT_VCF} ${NORMAL_READCOUNTS} ${TUMOUR_READCOUNTS}
